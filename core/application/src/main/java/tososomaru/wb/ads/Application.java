2858f567032a7d078c81ca050dc2282edf956419
Nikita Pivkin <nikita.pivkin@smartforce.io>
2023-01-09 14:53:24 +0600

    cleanup

--- core/application/src/main/java/tososomaru/wb/ads/Application.java
+++ core/application/src/main/java/tososomaru/wb/ads/Application.java
@@ -1,16 +1,10 @@
 package tososomaru.wb.ads;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-    @Value("${wb.x-supplier-id-external}")
-    String id;
-    @Value("${wb.WILDAUTHNEW_V3}")
-    String token;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
